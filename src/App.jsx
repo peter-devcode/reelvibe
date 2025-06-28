a5416267b1cb0ac47a8bf196aefcadef8d2228d9
peter-devcode <peter.muokwugwo@outlook.com>
2025-06-27 21:13:01 -0400

    add search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import Search from "./components/Search";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [search, setSearch] = useState("");
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <header>
+        <img src="./logo.svg" alt="reelvibe logo" />
+        <img src="./hero-img.png" alt="reelvibe hero" />
+        <h1>Find the Movies You'll Enjoy Without the Hassle.</h1>
+      </header>
 
-export default App
+      <main>
+        <section id="search">
+          <Search search={search} setSearch={setSearch} />
+        </section>
+        <section id="trending">Trending</section>
+        <section id="all-movie">All Movie</section>
+      </main>
+    </>
+  );
+};
+
+export default App;
