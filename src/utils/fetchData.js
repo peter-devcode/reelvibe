e62cf719ca7bead0e8aa5d85f34728b557c6ef35
peter-devcode <peter.muokwugwo@outlook.com>
2025-06-30 15:20:55 -0400

    update fetchData function to incorporate updateSearchCount

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,28 +1,34 @@
+import { updateSearchCount } from "./updateSearchCount";
+
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-const API_BASE_URL = 'https://api.themoviedb.org/3'
+const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_OPTIONS = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        Authorization: `Bearer ${API_KEY}`,
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
+
+export async function fetchData(query = "") {
+  try {
+    
+    const endpoint = query
+      ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+      : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+    const response = await fetch(endpoint, API_OPTIONS);
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch movies", response.status);
     }
-}
+    const data = await response.json();
 
-
-export async function fetchData() {
-    try {
-        const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
-        const response = await fetch(endpoint, API_OPTIONS);
-        
-
-        if (!response.ok) {
-            throw new Error("Failed to fetch movies", response.status);
-        }
-        const data = await response.json()
-        console.log(data.results)
-        return data.results;
-
-    } catch (error) {
-        console.error('Error fetching movies', error)
+    if (query && data.results.length > 0) {
+      await updateSearchCount(query, data.results[0])
     }
+    console.log(data.results);
+    return data.results;
+  } catch (error) {
+    console.error("Error fetching movies", error);
+  }
 }
